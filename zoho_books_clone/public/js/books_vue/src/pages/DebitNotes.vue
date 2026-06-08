4e010975c6a791b533470c1940c89119dcc35149
santha-ashwin-psd <santhaashwin@psdigitise.com>
2026-06-08 12:14:00 +0530

    debit note design fix

--- zoho_books_clone/public/js/books_vue/src/pages/DebitNotes.vue
+++ zoho_books_clone/public/js/books_vue/src/pages/DebitNotes.vue
@@ -102,7 +102,7 @@
       <div class="inv-dh">
         <div class="dn-dheader-left">
           <div class="dn-dheader-ico" :class="editingName?'edit':''">
-            <span v-html="icon(editingName?'edit':'debitnote',18)"></span>
+            <span><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg></span>
           </div>
           <div>
             <div class="inv-dh-title">{{ editingName ? 'Edit Debit Note' : 'New Debit Note' }}</div>
